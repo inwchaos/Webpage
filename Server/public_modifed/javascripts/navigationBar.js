6cecf44ff01f03c2c42a306ee359d62af675f86d
JarrenChov <jchov98@outlook.com>
2016-10-07 00:53:38 +1030

    modifed public file

---
+++ Server/public_modifed/javascripts/navigationBar.js
@@ -0,0 +1,9 @@
+// Script for allowing a menu bar to appear/dissapear from the sides
+
+function openNavigationBar() {
+    document.getElementById("navigationBar").style.width="100%";
+}
+
+function closeNavigationBar() {
+    document.getElementById("navigationBar").style.width="0";
+}				
