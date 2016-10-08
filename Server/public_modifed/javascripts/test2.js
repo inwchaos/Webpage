a54579fdd34a64889d5df10fd42c2e849476d30f
JarrenChov <jchov98@outlook.com>
2016-10-09 01:53:13 +1030

    removing all files to reupdate

--- Server/public_modifed/javascripts/test2.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-new_user_name = "User_x";
-new_user_pass = "Pass_x";
-
-var new_user = {username : new_user_name, password : new_user_pass};
-var json_string = JSON.stringify(new_user);
-
-fs.writeFile('user_database.json', json_string, 'utf8');
